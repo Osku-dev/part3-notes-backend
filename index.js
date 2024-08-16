cffdd9c961ae3487daf05d400e12df8a99f4bc9b
oskah <oskah@uef.fi>
2024-08-16 10:36:47 +0300

    testest

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -99,7 +100,7 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
